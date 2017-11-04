0d33889c64aa1934dbfcf1e8e320b36479ba53bd
Kevin Toro <kevintoro@ac.c4q.nyc>
2017-11-04 16:53:31 -0400

    Set new methods and implemented new logic

--- app/src/main/java/nyc/c4q/simonsays/SplashScreen.java
+++ app/src/main/java/nyc/c4q/simonsays/SplashScreen.java
@@ -1,16 +1,25 @@
 package nyc.c4q.simonsays;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashScreen extends AppCompatActivity {
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
         setContentView(R.layout.activity_splash_screen);
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashScreen.this,MainActivity.class);
+                SplashScreen.this.startActivity(mainIntent);
+                SplashScreen.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
     }
 }

eecb2b43fbf2e366c2baa0a0269a0173ff68bf05
Muhaimen Noor <muhaimennoor@ac.c4q.nyc>
2017-11-10 17:59:01 -0500

    Changed to constraint layout from relative layout.

--- app/src/main/java/nyc/c4q/simonsays/MainActivity.java
+++ app/src/main/java/nyc/c4q/simonsays/MainActivity.java
@@ -1,168 +1,27 @@
 package nyc.c4q.simonsays;
 
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.graphics.Color;
-import android.nfc.Tag;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
-import android.widget.Button;
-import android.widget.Toast;
+import android.widget.TextView;
 
-import java.sql.SQLOutput;
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.Random;
 public class MainActivity extends AppCompatActivity {
-    private Button red;
-    private Button yellow;
-    private Button blue;
-    private Button green;
-    private Button start;
-    private final String Tag="TestActivity";
-    private ArrayList<String> simon;
-    private ArrayList<String> user;
-    private boolean clicked;
+    private View red;
+    private View yellow;
+    private View blue;
+    private View green;
+    private TextView rounds;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.SplashTheme);
         super.onCreate(savedInstanceState);
-//        final Handler handler = new Handler();
-//        handler.postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                Intent
-//            }
-//        }, 3000);
+
         setContentView(R.layout.activity_main);
-        red = (Button) findViewById(R.id.red);
-        green = (Button) findViewById(R.id.green);
-        yellow = (Button) findViewById(R.id.button_Y);
-        blue = (Button) findViewById(R.id.blue);
-        start = (Button) findViewById(R.id.begin);
-
-        String checking = green.getText().toString();
-        Log.d("checkforgreen", checking);
-
+        red = findViewById(R.id.red);
+        green = findViewById(R.id.green);
+        yellow = findViewById(R.id.yellow);
+        blue = findViewById(R.id.blue);
+        rounds = (TextView) findViewById(R.id.rounds);
     }
-    public void onStart() {
-        super.onStart();
-        start.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                final Animation lightup= new AlphaAnimation(1,0);
-                lightup.setDuration(3000);
-                Toast.makeText(MainActivity.this, "Start button was clicked", Toast.LENGTH_SHORT).show();
-                Random rnd= new Random();
-                int[] randColor={R.color.blue,R.color.green,R.color.red,R.color.yellow,R.color.pink};
-                red.setBackground(blue.getBackground());
-                red.startAnimation(lightup);
-                yellow.setBackground(green.getBackground());
-                //green.setBackgroundColor(getResources().getColor(R.color.pink));
-            }
-        });
-        blue.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Blue clicked", Toast.LENGTH_SHORT).show();
-            }
-        });
-        red.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Red clicked", Toast.LENGTH_SHORT).show();
-            }
-        });
-        yellow.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Yellow clicked", Toast.LENGTH_SHORT).show();
-            }
-        });
-        green.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-
-                Toast.makeText(MainActivity.this, "green clicked", Toast.LENGTH_SHORT).show();
-                clicked = true;
-            }
-        });
-//        listCheck(simon, user);
-        gamePlay(simon, user);
-    }
-//    public boolean listCheck(ArrayList<String> simon, ArrayList<String> user) {
-//            if(clicked){
-//                Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show();
-//                simon.add(green.getText().toString());
-//                String checking = green.getText().toString();
-//                Log.d("checkforgreen", checking);
-//            }
-//
-//
-//        return clicked;
-//    }
-    public boolean gamePlay(ArrayList<String> simon, ArrayList<String> user) {
-        final Animation lightup= new AlphaAnimation(1,0);
-        lightup.setDuration(3000);
-        Random simonChoice = new Random();
-        int choice = simonChoice.nextInt(4);
-        if(clicked) {
-
-
-            switch (choice) {
-
-                case 1:
-                    simon.add(green.getText().toString());
-                    lightup.setDuration(3000);
-                    green.startAnimation(lightup);
-
-                    break;
-                case 2:
-                    simon.add(red.getText().toString());
-                    red.startAnimation(lightup);
-
-                    break;
-                case 3:
-                    simon.add(yellow.getText().toString());
-                    yellow.startAnimation(lightup);
-
-                    break;
-                case 4:
-                    simon.add(blue.getText().toString());
-                    blue.startAnimation(lightup);
-
-                    break;
-            }
-        }
-
-        return true;
-    }
-
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//TODO create button methods and logic, as well as key bindings, fields as well
-//TODO XML needs to be properly adjusted and designed, color schema,
-//TODO a second activity, that will be explicit intent, which will contain the Simon game
-//and the main_activity will be the splash page
---
+++ app/src/main/java/nyc/c4q/simonsays/SimonSaysPresenter.java
@@ -0,0 +1,8 @@
+package nyc.c4q.simonsays;
+
+/**
+ * Created by muhaimennoor on 11/10/17.
+ */
+
+public class SimonSaysPresenter {
+}
